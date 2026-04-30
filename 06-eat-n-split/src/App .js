847f94e18a6bf08de2deaeef52fbceb84e8a06bf
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-04-30 11:14:20 +0530

    new form friens=d added

---
+++ 06-eat-n-split/src/App .js
@@ -0,0 +1,125 @@
+import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        {showAddFriend && <FormAddFriend />}
+        <FormAddFriend />
+        <button onClick={handleShowAddFriend}>
+          {showAddFriend ? "close" : "Add friend"}
+        </button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3> {friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You owe {friend.name} are even</p>}
+
+      <button>Select</button>
+    </li>
+  );
+}
+function FormAddFriend() {
+  return (
+    <form className="form-add-friends">
+      <label>🧑‍🤝‍🧑 Friend name</label>
+      <input type="text" />
+
+      <label>📷Image URL</label>
+      <input type="text" />
+
+      <button> Add</button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰Bill Value</label>
+      <input type="text" />
+
+      <label>🙎Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑X's expense</label>
+      <input type="text" disabled />
+
+      <label>💰 Who is paying the bill?</label>
+      <select>
+        <option value="user"> You</option>
+        <option value="friend"> X</option>
+      </select>
+
+      <button>Split Bill</button>
+    </form>
+  );
+}
