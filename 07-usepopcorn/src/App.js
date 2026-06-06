8da663460fbbb212ee400c5b7fffb1857fb23dfb
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-06 14:17:03 +0530

    async await function

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -55,11 +55,19 @@ const KEY = "aa740ee4";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "interstellar";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      console.log(data.Search);
+    }
+
+    fetchMovies();
   }, []);
 
   return (
