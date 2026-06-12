ca3ab0f47d73801a386ab8c3ed483c0bde9f2b16
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-12 17:47:06 +0530

    adding a loading state

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -55,24 +55,23 @@ const KEY = "aa740ee4";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
       );
       const data = await res.json();
       setMovies(data.Search);
-      console.log(data.Search);
+      setIsLoading(false);
     }
 
     fetchMovies();
   }, []);
 
-  /////
-  ///ghhuftye6fyugy
-  //hjgyryufuygf
   return (
     <>
       <NavBar>
@@ -81,20 +80,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        {/* <Box element={<MovieList movies={movies} />} />
-
-        <Box
-          element={
-            <>
-              <WatchedSummary watched={watched} />
-              <WatchedMovieList watched={watched} />
-            </>
-          }
-        /> */}
-
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -103,6 +89,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
