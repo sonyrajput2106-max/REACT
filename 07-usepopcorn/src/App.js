8a7738dc50844ced8630a85443da730c4de04eb2
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-23 14:13:32 +0530

    Listening to keypress

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -54,7 +54,7 @@ const average = (arr) =>
 const KEY = "aa740ee4";
 
 export default function App() {
-  const [query, setQuery] = useState("inception");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -114,9 +114,8 @@ export default function App() {
 
           setMovies(data.Search);
         } catch (err) {
-          console.error(err.message);
-
           if (err.name !== "AbortError") {
+            console.log(error(err.message));
             setError(err.message);
           }
         } finally {
@@ -124,6 +123,12 @@ export default function App() {
         }
       }
 
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      handleCloseMovie();
       fetchMovies();
 
       return function () {
@@ -330,6 +335,23 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie],
+  );
 
   useEffect(
     function () {
