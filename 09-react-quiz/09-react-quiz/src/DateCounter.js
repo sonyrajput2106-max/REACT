96711f8742cd7bf0d49c14a7c8261f54f1e25f89
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-07-07 18:23:32 +0530

    manages related pieces of state

--- 09-react-quiz/09-react-quiz/src/DateCounter.js
+++ 09-react-quiz/09-react-quiz/src/DateCounter.js
@@ -1,15 +1,19 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
 
   switch (action.type) {
     case "dec":
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - state.step };
     case "inc":
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + state.step };
     case "setCount":
       return { ...state, count: action.payload };
+    case "reset":
+      return initialState;
 
     default:
       throw new Error("Unknown action");
@@ -17,10 +21,6 @@ function reducer(state, action) {
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  // const [step, setStep] = useState(1);
-
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -30,31 +30,22 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: "dec" });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
   };
 
   const inc = function () {
     dispatch({ type: "inc" });
-
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
     dispatch({ type: "setCount ", payload: Number(e.target.value) });
-
-    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
     dispatch({ type: "setStep", payload: Number(e.target.value) });
-    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    // setCount(0);
-    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
