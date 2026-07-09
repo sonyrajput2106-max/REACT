a736eddc80884262c8a7a5f6cea41e658811ede7
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-07-09 23:16:19 +0530

    loaded questions

--- 09-react-quiz/09-react-quiz/src/App.js
+++ 09-react-quiz/09-react-quiz/src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error"));
+  }, []);
   return (
     <div className="app">
       <Header />
@@ -13,5 +20,3 @@ export default function App() {
     </div>
   );
 }
-//fhithfuieregh
-//gkhguihdf
