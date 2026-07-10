3b9f65493e6c596be436af62889e2110e7942042
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-07-10 23:40:07 +0530

    displayed questions

--- 09-react-quiz/09-react-quiz/src/components/App.js
+++ 09-react-quiz/09-react-quiz/src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
 
   //'loading' , 'error' , 'ready' , 'active' , 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -36,7 +37,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   const numQuestions = questions.length;
 
@@ -56,13 +60,8 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
 }
-
-//edrftghjk
-//qwsedrftgyhu
-//wqerdtfgyhuASDFGHJdfghdgfhjfghdfgh
-//sdfghjjkfd
